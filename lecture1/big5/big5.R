ff176a5dd6ab91af511ab47161361244434ca786
Alexander <51855086+akrothschild@users.noreply.github.com>
2024-07-03 11:09:31 +0200

    make more beautiful

--- lecture1/big5/big5.R
+++ lecture1/big5/big5.R
@@ -3,16 +3,24 @@
 # 
 # # Problem to Solve
 # 
-# Ever taken a BuzzFeed quiz to determine whether you’re more like a brownie or a chocolate chip cookie? Turns out personality can be characterized in many ways and—within the present-day psychological community—some of the most common traits to describe personality include:
+# Ever taken a BuzzFeed quiz to determine whether you’re more like a brownie or 
+# a chocolate chip cookie? Turns out personality can be characterized in many 
+# ways and—within the present-day psychological community—some of the most 
+# common traits to describe personality include:
 #   
-# Extroversion, the extent to which one might be socially outgoing
-# Neuroticism, the extent to which one might experience emotional swings
-# Agreeableness, the extent to which one might seek to be cooperative and empathetic
-# Conscientiousness, the extent to which one might prioritize order and self-discipline
-# Openness, the extent to which one might be open to new experiences
-# These 5 personality traits are together referred to as “The Big 5.” Psychologists (or those who are just curious about their personality!) might use various personality tests to assess the relative strength of these traits in one’s personality.
+# - Extroversion, the extent to which one might be socially outgoing
+# - Neuroticism, the extent to which one might experience emotional swings
+# - Agreeableness, the extent to which one might seek to be cooperative and empathetic
+# - Conscientiousness, the extent to which one might prioritize order and self-discipline
+# - Openness, the extent to which one might be open to new experiences
 # 
-# In a program called big5.R, in a folder called big5, write a program to analyze the results of thousands of Big 5 personality tests.
+# These 5 personality traits are together referred to as “The Big 5.” 
+# Psychologists (or those who are just curious about their personality!) might 
+# use various personality tests to assess the relative strength of these traits 
+# in one’s personality.
+# 
+# In a program called big5.R, in a folder called big5, write a program to analyze 
+# the results of thousands of Big 5 personality tests.
 #  
 # ========================
 
--- lecture1/pitstop/pitstop.R
+++ lecture1/pitstop/pitstop.R
@@ -23,22 +23,22 @@
 # ========================
 
 print("whats your set?")
-set_name <- readline()
+set_name              <- readline()
 
 # ========================
 #  CREATE FUNCTION
 #  to analyze the pit stops
 # ========================
 
-analyze <- function(set_path) {
-  pitstops <- read.csv(set_path)
-  pitstops_number <- nrow(pitstops)
-  pitstops_shortest <- min(pitstops$time)
-  pitstops_longest <- max(pitstops$time)
-  pitstops_time_spent <- sum(pitstops$time)
-  cat("Total pitstops:", pitstops_number, "\n")
-  cat("Shortest pitstop:", pitstops_shortest, "\n")
-  cat("Longest pitstop:", pitstops_longest, "\n")
+analyze               <- function(set_path) {
+  pitstops              <- read.csv(set_path)
+  pitstops_number       <- nrow(pitstops)
+  pitstops_shortest     <- min(pitstops$time)
+  pitstops_longest      <- max(pitstops$time)
+  pitstops_time_spent   <- sum(pitstops$time)
+  cat("Total pitstops:",         pitstops_number,     "\n")
+  cat("Shortest pitstop:",       pitstops_shortest,   "\n")
+  cat("Longest pitstop:",        pitstops_longest,    "\n")
   cat("Time spent on pitstops:", pitstops_time_spent, "\n")
 }
 
