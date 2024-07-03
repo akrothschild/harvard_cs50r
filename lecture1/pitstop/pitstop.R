3a002ca6a3ea279de06776e6b3123683575f18f5
Alexander <51855086+akrothschild@users.noreply.github.com>
2024-07-03 10:54:16 +0200

    CHANGE PITSTOP

--- lecture1/pitstop/pitstop.R
+++ lecture1/pitstop/pitstop.R
@@ -1,6 +1,34 @@
-print("whats your set?")
+# ========================
+#         PIT STOP  
+# 
+# Problem to Solve
+# 
+# Ever changed your car’s tires in less than 3 seconds? 
+# In Formula One, that’s the usual!
+#   
+# Formula One is a type of elite, international racing. 
+# Races take place all over the world, including destinations like Miami, 
+# Florida, and Suzuka, Japan. Besides the racer themselves, each team is 
+# composed of a “pit crew” of up to 22 people. The pit crew changes tires, 
+# fixes the car, and refuels very quickly. Nowadays, an excellent pit stop can 
+# take less than 3 seconds.
+# 
+# In a program called pitstop.R, in a folder called pitstop, write a program 
+# to analyze pit stop times at Formula One races.
+#
+# ========================
 
+# ========================
+#  READ USER´S SET        
+# ========================
+
+print("whats your set?")
 set_name <- readline()
+
+# ========================
+#  CREATE FUNCTION
+#  to analyze the pit stops
+# ========================
 
 analyze <- function(set_path) {
   pitstops <- read.csv(set_path)
@@ -13,5 +41,9 @@ analyze <- function(set_path) {
   cat("Longest pitstop:", pitstops_longest, "\n")
   cat("Time spent on pitstops:", pitstops_time_spent, "\n")
 }
+
+# ========================
+#  LAUNCH THE FUNCTION        
+# ========================
 
 analyze(set_name)
